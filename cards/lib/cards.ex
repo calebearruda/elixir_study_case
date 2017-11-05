95b4db5b3ea0d466df2c5248d4ada1b8b899f606
Calebe Arruda <calebearruda@gmail.com>
2017-11-05 20:44:13 -0200

    Created the method create_hand and using pipeline

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -32,4 +32,10 @@ defmodule Cards do
        {:error, reason} -> "An error occour because: #{:file.format_error(reason)}"
     end
   end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+      |> Cards.shuffle
+      |> Cards.deal(hand_size)
+  end
 end
