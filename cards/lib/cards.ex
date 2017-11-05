eda5366eaef64c30f286f67c835ea5d76ff76d88
Calebe Arruda <calebearruda@gmail.com>
2017-11-05 11:06:38 -0200

    First commit, init repository

---
+++ cards/lib/cards.ex
@@ -0,0 +1,14 @@
+defmodule Cards do
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    
+  end
+end
