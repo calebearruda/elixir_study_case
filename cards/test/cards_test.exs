5824b7d26129b95ad26ac6323aff8320882631ca
Calebe Arruda <calebearruda@gmail.com>
2017-11-06 22:05:14 -0200

    Created test cases, using test normal and doctest

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,22 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
   end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
+
+  test "save a deck and load a deck" do
+    deck = Cards.create_deck
+    Cards.save deck, "test_deck_saved"
+
+    deck_loaded = Cards.load "test_deck_saved"
+    assert deck == deck_loaded
+  end
+
 end
